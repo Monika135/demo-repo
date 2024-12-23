e6ae5efbfdd16e08ef2ddae206b347765a03c807
Monika <11613.mona@gmail.com>
2024-12-23 18:14:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const age = 18
+const fname = "xyz"
+
+function getname(){
+    console.log("age "+ age + " and name is "+fname)
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+const age = 18
+const name = "xyz"
+
+function eligible(){
+    if(age>=18){
+        console.log("eligible")
+    }
+    else{
+        console.log("not eligible")
+    }
+}
